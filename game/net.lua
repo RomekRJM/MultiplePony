6d789620442e27d2c531f177d83d40b1b875f75a
Roman Subik <romek.rjm@gmail.com>
2024-06-10 09:14:15 +0200

    Send player id to server.

--- game/net.lua
+++ game/net.lua
@@ -1,12 +1,14 @@
 room_id_addr = 0x5f80 -- index 0
-player_score_delta = 0x5f81 -- index 1
-player_score_timestamp = 0x5f82 -- index 2
+player_id_addr = 0x5f81 -- index 1
+player_score_delta_addr = 0x5f82 -- index 2
+player_score_timestamp_addr = 0x5f83 -- index 3
 
 function establishConnection()
-    poke(room_id_addr, 0) -- hard code to 0
+    poke(room_id_addr, 0) -- hard code to room 0
 end
 
 function sendScore(scoreDelta, timestamp)
-    poke(player_score_delta, scoreDelta)
-    poke(player_score_timestamp, timestamp)
+    poke(player_id_addr, 0) -- hard code to player 0
+    poke(player_score_delta_addr, scoreDelta)
+    poke(player_score_timestamp_addr, timestamp)
 end
