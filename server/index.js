d49b2013a7b143ac5ca43500112d6c97af38a502
Roman Subik <roman.subik@ocado.com>
2024-07-10 06:32:34 +0200

    Assign players to rooms.

--- server/index.js
+++ server/index.js
@@ -1,70 +1,123 @@
-const { createPicoSocketServer } = require("pico-socket");
-const { dispatch_request } = require("./request_dispatcher");
+const {createPicoSocketServer} = require("pico-socket");
+const {dispatch_request} = require("./request_dispatcher");
 
 const {app, server, io} = createPicoSocketServer({
-  assetFilesPath: ".",
-  htmlGameFilePath: "./server/game.html",
+    assetFilesPath: ".",
+    htmlGameFilePath: "./server/game.html",
 
-  clientConfig: {
-    roomIdIndex: 0, // ROOM_ID
+    clientConfig: {
+        roomIdIndex: 0, // ROOM_ID
 
-    // index to determine the player id
-    playerIdIndex: 1, // PLAYER_ID
+        // index to determine the player id
+        playerIdIndex: 1, // PLAYER_ID
 
-    // each player has: score_delta, timestamp
-    playerDataIndicies: [
-      [2, 3], // PLAYER_0
-      [3, 4], // PLAYER_1
-    ],
-  },
+        // each player has: score_delta, timestamp
+        playerDataIndicies: [
+            [2, 3], // PLAYER_0
+            [3, 4], // PLAYER_1
+        ],
+    },
 });
 
 const logData = (data) => {
-  const emptyArray = new Array(data.length);
-  data.forEach((element, index) => {
-    if (element !== null) {
-      emptyArray[index] = element;
-    }
-  });
-  return emptyArray;
+    const emptyArray = new Array(data.length);
+    data.forEach((element, index) => {
+        if (element !== null) {
+            emptyArray[index] = element;
+        }
+    });
+    return emptyArray;
 };
 
 const handleConnection = (data) => {
-  const roomId = data[0];
+    const roomId = data[0];
 
-  if (roomId !== 0) {
-    return;
-  }
+    if (roomId !== 0) {
+        return;
+    }
 
-  console.log(String.fromCharCode(data.slice(1)));
+    console.log(String.fromCharCode(data.slice(1)));
 
 };
 
+const maxPlayersInTeam = 5;
+const maxPlayersInRoom = 2 * maxPlayersInTeam;
+
+const roomData = [
+    {
+        team1Players: [],
+        team2Players: [],
+        gameInProgress: false,
+    },
+    {
+        team1Players: [],
+        team2Players: [],
+        gameInProgress: false,
+    },
+    {
+        team1Players: [],
+        team2Players: [],
+        gameInProgress: false,
+    },
+];
+
+const assignToRoomAndTeam = (playerId) => {
+    let roomIdToJoin = roomData.findIndex((room) => {
+        if (room.gameInProgress) {
+            return false;
+        }
+
+        return room.team1Players.length + room.team2Players.length < maxPlayersInRoom;
+    });
+
+    if (roomIdToJoin === -1) {
+        return null;
+    }
+
+    let roomToJoin = roomData[roomIdToJoin];
+    let teamToJoin;
+
+    if (roomToJoin.team1Players.length > roomToJoin.team2Players.length) {
+        teamToJoin = 2;
+        roomToJoin.team2Players.push(playerId);
+    } else {
+        teamToJoin = 1;
+        roomToJoin.team1Players.push(playerId);
+    }
+
+    return {
+        roomId: roomIdToJoin,
+        team: teamToJoin,
+    };
+}
+
 // replace default logic
-io.removeAllListeners("connection")
+io.removeAllListeners("connection");
 
 io.on("connection", (socket) => {
-  // save a `roomId` variable for this socket connection
-  // when sending / recieving data, it will only go to people in the same room
-  let roomId;
-  socket.on("disconnect", () => {});
-  // attach a room id to the socket connection
-  socket.on("room_join", (evtData) => {
-    handleConnection(evtData);
-    socket.join(evtData.roomId);
-    roomId = evtData.roomId;
+    // save a `roomId` variable for this socket connection
+    // when sending / recieving data, it will only go to people in the same room
+    let roomId;
+    socket.on("disconnect", () => {
+    });
+    // attach a room id to the socket connection
+    socket.on("room_join", (evtData) => {
+        let playerAssignment = assignToRoomAndTeam(evtData)
+        handleConnection(evtData);
+        socket.join(evtData.roomId);
+        roomId = evtData.roomId;
 
-    // if DEBUG=true, log when clients join
-    if (process.env.DEBUG) {
-      console.log("pony joined room: ", roomId);
-    }
-  });
+        // if DEBUG=true, log when clients join
+        if (process.env.DEBUG) {
+            console.log("pony joined room: ", playerAssignment.roomId, ", team: ", playerAssignment.team);
+        }
+    });
 
-  socket.on("update", (payload) => {
-    dispatch_request(socket, payload);
+    socket.on("update", (payload) => {
+        dispatch_request(socket, payload);
 
-    if (process.env.DEBUG) {
-      console.log(`${roomId}: `, logData(payload));
-    }
-  });
+        if (process.env.DEBUG) {
+            console.log(`${roomId}: `, logData(payload));
+        }
+    });
 });
