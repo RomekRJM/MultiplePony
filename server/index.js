bec4b3968c4c7a0d7e0bf8d7911f6b8e0a0a8ec1
Roman Subik <roman.subik@ocado.com>
2024-07-30 12:58:18 +0200

    Test multiple connections.

--- server/index.js
+++ server/index.js
@@ -91,6 +91,18 @@ io.on("connection", (socket) => {
     let roomId;
     socket.on("disconnect", () => {
     });
+
+    socket.on("RESET", () => {
+        // hack until disconnection is properly handled
+        roomData.forEach((room) => {
+            room.team1Players = [];
+            room.team2Players = [];
+            room.gameInProgress = false;
+        });
+        console.log("RESET received, rooms now empty");
+
+        socket.emit("RESETED_ROOMS");
+    })
     // attach a room id to the socket connection
     socket.on("JOIN_SERVER_CMD", (playerName) => {
         let playerAssignment = assignToRoomAndTeam(playerName);
--- server/tests.spec.js
+++ server/tests.spec.js
@@ -1,7 +1,7 @@
 import { beforeAll, afterAll, describe, it, expect } from "vitest";
 import { io as ioc } from "socket.io-client";
 
-describe("multiple pony server", () => {
+describe.sequential("multiple pony server", () => {
     let io, clientSocket;
 
     beforeAll(() => {
@@ -18,28 +18,47 @@ describe("multiple pony server", () => {
 
     it("should allow player to connect", () => {
         return new Promise((resolve) => {
-            let p1Name = 'PLAYER1';
-            clientSocket.emit("JOIN_SERVER_CMD", p1Name);
+            clientSocket.emit("RESET");
+            clientSocket.on("RESETED_ROOMS", () => {
+                let p1Name = 'PLAYER1';
+                clientSocket.emit("JOIN_SERVER_CMD", p1Name);
 
-            clientSocket.on("CONNECTED_TO_SERVER_RESP", ({roomId, playerId}) => {
-                expect(playerId).toEqual(0);
-                expect(roomId).toEqual(0);
-                resolve();
+                clientSocket.on("CONNECTED_TO_SERVER_RESP", ({roomId, playerId}) => {
+                    expect(playerId).toEqual(0);
+                    expect(roomId).toEqual(0);
+                    resolve();
+                });
             });
         });
     });
 
-    it("should update player on team changes", () => {
+    it("should update player on team changes with 1 player", () => {
+        return connectPlayers(1, {team1Players: ['0'], team2Players: []});
+    });
+
+    it("should update player on team changes with 4 player(s)", () => {
+        return connectPlayers(4, {team1Players: ['0', '2'], team2Players: ['1', '3']});
+    });
+
+    const connectPlayers = (noPlayers, expectedTeamNamesResponse) => {
         return new Promise((resolve) => {
-            let p1Name = 'PLAYER1';
-            let p2Name = 'PLAYER2';
-            clientSocket.emit("JOIN_SERVER_CMD", p1Name);
-            clientSocket.emit("JOIN_SERVER_CMD", p2Name);
+            clientSocket.emit("RESET");
+            clientSocket.on("RESETED_ROOMS", () => {
+                let count = 0;
+                for (let i = 0; i < noPlayers; i++) {
+                    clientSocket.emit("JOIN_SERVER_CMD", i.toString());
+                    console.log(i);
+                }
 
-            clientSocket.on("UPDATE_TEAM_NAMES", (teams) => {
-                expect(teams).toEqual({team1Players: [p1Name], team2Players: [p2Name]});
-                resolve();
+                clientSocket.on("UPDATE_TEAM_NAMES", (teams) => {
+                    count += 1;
+
+                    if (count === noPlayers) {
+                        expect(teams).toEqual(expectedTeamNamesResponse);
+                        resolve();
+                    }
+                });
             });
         });
-    });
+    }
 });
