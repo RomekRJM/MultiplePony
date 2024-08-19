e777b44d8370279df12d77ae20832b7d5608f355
Roman Subik <rjm_20@o2.pl>
2024-08-19 08:31:59 +0200

    Fix GPIO write code.

--- game/net.lua
+++ game/net.lua
@@ -1,4 +1,4 @@
-command_index = 0                -- index 0
+command_index = 1                -- index 0
 -- player_id_addr = 0x5f81              -- index 1
 -- player_score_delta_addr = 0x5f82     -- index 2
 -- player_score_timestamp_addr = 0x5f83 -- index 3
@@ -19,6 +19,7 @@ end
 function createEmptyPayload()
   local payload = {}
   for i = 1, GPIO_LENGTH do
+
     payload[i] = 0
   end
 
@@ -32,7 +33,7 @@ function establishConnection()
   payload[command_index] = JOIN_SERVER_CMD;
 
   for i = 1, #playerName do
-    payload[i + 2] = ord(playerName, counter)
+    payload[i + 2] = ord(playerName, i)
   end
 
   sendBuffer(payload)
@@ -42,4 +43,16 @@ function sendBuffer(payload)
   for i = 1, #payload do
     poke(BROWSER_GPIO_START_ADDR - 1 + i, payload[i])
   end
+end
+
+function dbgbuff()
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 0)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 1)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 2)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 3)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 4)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 5)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 6)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 127)))
+  print(tostring(peek(BROWSER_GPIO_START_ADDR + 128)))
 end
