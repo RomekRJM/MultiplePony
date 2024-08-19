4ef571065314f09b29832357ea7520f99a37865a
Roman Subik <roman.subik@ocado.com>
2024-08-19 09:49:12 +0200

    Attempt to pass data from browser to pico.

--- game/net.lua
+++ game/net.lua
@@ -1,4 +1,4 @@
-command_index = 1                -- index 0
+COMMAND_INDEX = 1
 -- player_id_addr = 0x5f81              -- index 1
 -- player_score_delta_addr = 0x5f82     -- index 2
 -- player_score_timestamp_addr = 0x5f83 -- index 3
@@ -7,7 +7,8 @@ command_index = 1                -- index 0
 BROWSER_GPIO_START_ADDR = 0x5f80
 BROWSER_GPIO_END_ADDR = 0x5fff
 
-JOIN_SERVER_CMD = 255
+JOIN_SERVER_CMD = 1
+CONNECTED_TO_SERVER_RESP = 255
 GPIO_LENGTH = 128
 
 function clearGPIOPins()
@@ -30,7 +31,7 @@ function establishConnection()
   local playerName = "BAR"
   local payload = createEmptyPayload()
 
-  payload[command_index] = JOIN_SERVER_CMD;
+  payload[COMMAND_INDEX] = JOIN_SERVER_CMD;
 
   for i = 1, #playerName do
     payload[i + 1] = ord(playerName, i)
@@ -43,6 +44,30 @@ function sendBuffer(payload)
   for i = 1, #payload do
     poke(BROWSER_GPIO_START_ADDR - 1 + i, payload[i])
   end
+end
+
+function handleConnectedToServer()
+  local room = peek(BROWSER_GPIO_START_ADDR + 1)
+  local playerId = peek(BROWSER_GPIO_START_ADDR + 2)
+  local admin = peek(BROWSER_GPIO_START_ADDR + 3)
+  local team = peek(BROWSER_GPIO_START_ADDR + 4)
+
+  print("Connected, room: " .. tostring(room) .. ", player id: " .. tostring(playerId) .. ", admin: " .. tostring(admin) .. ", team: " .. tostring(team))
+end
+
+command_lookup = {
+  [CONNECTED_TO_SERVER_RESP] = handleConnectedToServer
+}
+
+function handleBuffer()
+  local command = peek(BROWSER_GPIO_START_ADDR)
+
+  if command < 128 then
+    return
+  end
+
+  return command_lookup[command]()
+
 end
 
 function dbgbuff()
--- server/client.js
+++ server/client.js
@@ -4,25 +4,44 @@
  */
 
 const createPicoSocketClient = () => {
+    let player = {
+        id: -1,
+        token: "4567",
+        name: "",
+        team: -1,
+        roomId: -1,
+        admin: false
+    };
     const clientSocket = io.connect("http://localhost:5000/",
         {
             auth: {
-                token: "4567"
+                token: player.token
             }
-        })
+        });
     const commandIndex = 0;
-    const joinServerCommand = 255;
+    const roomIdIndex = 1;
+    const joinServerCommand = 1;
+    const connectToServerResponse = 255;
 
     const connectToRoomInterval = setInterval(() => {
         const command = window.pico8_gpio[commandIndex];
 
         if (command === joinServerCommand) {
-            const playerName = window.pico8_gpio.slice(1, 3).map((i) => String.fromCharCode(i)).join("");
+            player.name = window.pico8_gpio.slice(1,12).reduce((a, c) => a + String.fromCharCode(c), "");
             clearInterval(connectToRoomInterval);
-            clientSocket.emit("JOIN_SERVER_CMD", playerName);
+            clientSocket.emit("JOIN_SERVER_CMD", player.name);
 
             clientSocket.on("CONNECTED_TO_SERVER_RESP", ({roomId, playerId, team, admin}) => {
                 console.log("Connected to server", {roomId, playerId, team, admin});
+                player.roomId = roomId;
+                player.team = team;
+                player.admin = admin;
+
+                window.pico8_gpio[commandIndex] = connectToServerResponse;
+                window.pico8_gpio[roomIdIndex] = player.roomId;
+                window.pico8_gpio[2] = player.id;
+                window.pico8_gpio[3] = player.admin ? 1 : 0;
+                window.pico8_gpio[4] = player.team;
             });
             // window.requestAnimationFrame(onFrameUpdate);
         }
