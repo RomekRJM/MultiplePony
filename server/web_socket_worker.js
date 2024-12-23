fcb37ac5d964c5a86e4020d7e502c3833d480f2e
RomekRJM <roman.subik@ocado.com>
2024-12-23 07:50:01 +0100

    Extract web socket communication to WebWorker.

--- server/web_socket_worker.js
+++ server/web_socket_worker.js
@@ -1,13 +1,28 @@
-let i = 0;
+importScripts("../socket.io/socket.io.js");
 
-function timedCount() {
-  i = i + 1;
-  postMessage(i);
-  setTimeout("timedCount()",500);
-}
-
-self.addEventListener("message", function(e) {
-  console.log('in worker: ' + JSON.stringify(e.data));
+const clientSocket = io.connect(SERVER_URL || "http://localhost:5000/",
+{
+    auth: {
+        token: "CURRENT_PLAYER_NAME"
+    }
 });
 
-timedCount();
+self.addEventListener("message", function(e) {
+    clientSocket.emit(e.data.message, e.data.payload);
+});
+
+clientSocket.on("CONNECTED_TO_SERVER_RESP", ({roomId, playerId, team, admin}) => {
+    postMessage({message: "CONNECTED_TO_SERVER_RESP", payload: {roomId, playerId, team, admin}});
+});
+
+clientSocket.on("UPDATE_ROUND_PROGRESS_CMD", ({playerScores, winningTeam, clock, lastScoreUpdate}) => {
+    postMessage({message: "UPDATE_ROUND_PROGRESS_CMD", payload: {playerScores, winningTeam, clock, lastScoreUpdate}});
+});
+
+clientSocket.on("UPDATE_TEAM_NAMES", ({team1Name, team2Name}) => {
+    postMessage({message: "UPDATE_TEAM_NAMES", payload: {team1Name, team2Name}});
+});
+
+clientSocket.on("START_ROUND_COUNTDOWN_CMD", ({roundId, countdown}) => {
+    postMessage({message: "START_ROUND_COUNTDOWN_CMD", payload: {roundId, countdown}});
+});
