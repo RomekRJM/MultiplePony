ea8c80a82adc513ebd4cf2ec9e8163c1cd82111e
RomekRJM <romek.rjm@gmail.com>
2025-01-13 11:58:57 +0100

    End game screen: clearly state who won.

--- game/end.lua
+++ game/end.lua
@@ -36,13 +36,26 @@ end
 function drawEndScreen()
     pal()
 
+    local msg = ''
+    local msgCol = 10
+    local isWinner = false
+    local msgX = 58
+
     if room.team1Score == room.team2Score then
-        print('draw', 58, 8)
+        msg = 'draw'
     elseif room.team1Score > room.team2Score then
-        print('blue won', 46, 8)
+        isWinner = myself.team == 1
+        msg = 'blue won, ' .. (isWinner and 'bravo!!!' or 'sorry :(')
+        msgX = 38
+        msgCol = isWinner and 11 or 8
     else
-        print('red won', 50, 8)
+        isWinner = myself.team == 2
+        msg = 'red won, ' .. (isWinner and 'bravo!!!' or 'sorry :(')
+        msgX = 38
+        msgCol = isWinner and 11 or 8
     end
+
+    print(msg, msgX, 8, msgCol)
 
     for i, p in ipairs(leaderBoard) do
         print(tostring(i) .. '. ' .. p.name .. ' ' .. p.score, 38, 16 + i * 8, p.id == myself.id and 11 or 6)
